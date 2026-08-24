a1cc5549ee7c2f251fc895125fcdd3a16a1d67d5
David Griego <digri808@learner.dvusd.org>
2026-08-24 13:02:05 -0700

    Beh

--- python/main.py
+++ python/main.py
@@ -1,7 +1,11 @@
 # BCH Software Inc. - Sprint 1: Interactive Kiosk
 # Track: Python Software Engineering
+<<<<<<< HEAD
 # 8/20
 
+=======
+# 8/20 
+>>>>>>> origin/main
 def main():
     print("========================================")
     print("      BCH ENTERPRISE VISITOR KIOSK      ")
@@ -12,3 +16,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    def card():
+#Print ------------------------------------------------------------
+#Print Apex Vistor Check-In
+#Print ------------------------------------------------------------
+#Print Name:()
+#Input a name
+#Store previous input in ()
+#Print Name:(Stored Input)
+#Print Organization:()
+#Input a company
+#Store previous input in ()
+#Print Organization:(Stored Input)
+#Print Email:()
+#Input a Email
+#Store previous input in ()
+#Print Email:(Stored Input)
+#Print Badge Tier:()
+#Input the Badge Tier
+#Store previous input in ()
+#Print Badge Tier:(Stored Input)
+print (card)
